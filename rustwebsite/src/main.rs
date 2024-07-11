f0da0f7f47fd1ac6eda16914570365bde29ab961
Pedro <pjg2@pdx.edu>
2024-07-11 14:27:52 -0700

    adding changes

--- rustwebsite/src/main.rs
+++ rustwebsite/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::fs::{FileServer, NamedFile};
+use std::path::{Path, PathBuf};
+
+#[get("/")]
+async fn index() -> Option<NamedFile> {
+    NamedFile::open("frontend/static/index.html").await.ok()
+}
+
+#[get("/<file..>", rank = 2)]
+async fn static_files(file: PathBuf) -> Option<NamedFile> {
+    let path = Path::new("frontend/static/").join(file);
+    println!("{path:?}");
+    NamedFile::open(path).await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, static_files])
+        .mount("/frontend/static", FileServer::from("frontend/static"))
 }
